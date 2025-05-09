04bfed41c5f1e3e86565bdb1c78516b9be393e1c
Slowaq <wbaluchaw@gmail.com>
2025-05-09 18:46:41 +0200

    First initialization

---
+++ recog/knn.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def setup_arg_parser():
+    parser = argparse.ArgumentParser(description='Learn and classify image data.')
+    parser.add_argument('train_path', type=str, help='path to the training data directory')
+    parser.add_argument('test_path', type=str, help='path to the testing data directory')
+    parser.add_argument('-k', type=int, 
+                        help='run k-NN classifier (if k is 0 the code may decide about proper K by itself')
+    parser.add_argument("-o", metavar='filepath', 
+                        default='classification.dsv',
+                        help="path (including the filename) of the output .dsv file with the results")
+    return parser
+
+
+def main():
+    parser = setup_arg_parser()
+    args = parser.parse_args()
+    
+    print('Training data directory:', args.train_path)
+    print('Testing data directory:', args.test_path)
+    print('Output file:', args.o)
+    
+    print(f"Running k-NN classifier with k={args.k}")
+    
+    # TODO Train and test the k-NN classifier
+        
+        
+if __name__ == "__main__":
+    main()
+    
